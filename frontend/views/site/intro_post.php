be3b08c51671b72695b5f84f778d4141417a5f49
Nursultan Mussabayev <nursalga@gmail.com>
2017-01-23 17:40:51 +0600

    сделал представление колонок программирования на главном экране, нужно сделать представление на странице программирование

--- frontend/views/site/intro_post.php
+++ frontend/views/site/intro_post.php
@@ -1,16 +1,23 @@
 <div class="swiper-slide">
 <article class="prog" style='height: 100%'>
-    <div >
-    <a href="<?= $post->link?>">
-        <?=$post->entry_image?>
-    </a>
-        <div>
-	        <a href="<?= $post->link?>">
-	            <h5><?=$post->title?></h5>
-                <p><?=$post->entry_title_description?></p>
-	        </a>
-        </div>
-
+    <div class='entry_image'>
+	    <a href="<?= $post->link?>">
+	        <?=$post->entry_image?>
+	    </a>
+    </div>
+    <div class="prog_text">
+        <a href="<?= $post->link?>">
+            <h5>
+            	<span style="min-height: 48px; font-family: 'Roboto Slab'; display: block;"><?=$post->title?></span>
+            	<?php 
+            	if($post->entry_image == '<span></span>'){
+            		?>
+            		<div class='title_description'><?=$post->entry_title_description?></div> 
+        		<?php
+        		}?>
+            	
+            </h5>
+        </a>
     </div>
 </article>
 </div>
